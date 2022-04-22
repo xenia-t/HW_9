5e3ab4fe970d84dfa9be2c1af18a22121c862eaa
Xenia <listk98@yandex.ru>
2022-04-22 16:34:38 +0300

    Add Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N используя рекурсию.
+
+   M = 1; N = 5 -> "2, 4"
+   M = 4; N = 8 -> "4, 6, 8" */
+
+
+int M = Promt("\nВведите значение  числа M: ");
+int N = Promt("Введите значение числа N: ");
+Console.Write($"\nЧетные числа в промежутке [{M}, {N}] => "); 
+while (M <= 0) M +=1;
+EvenRow(M, N);
+
+
+   int Promt(String message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
+void EvenRow (int M, int N)
+{
+    if (M > N) return;
+    if (M % 2 == 0) Console.Write(M + " ");
+    EvenRow(M + 1, N);
+}
